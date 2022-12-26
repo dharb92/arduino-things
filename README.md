07276a1c68d16964a3f68ac07b7415e21c71dabf
Dean Harber <dean@codeswop.com>
2022-12-27 00:44:14 +0200

    Init repo with a example shift register LED animation

---
+++ README.md
@@ -0,0 +1,9 @@
+### Useful Arduino Libraries, Utilities and Examples
+
+#### Libraries and utilities
+
+A collection of libraries you can use in new projects.
+
+#### Examples
+
+Examples of how to use the libraries individually or together with other libraries.
---
+++ examples/led-74HC595-animation/led-74HC595-animation.ino
@@ -0,0 +1,35 @@
+int DATA = 8;
+int LATCH = 9;
+int CLOCK = 10;
+
+boolean registers[8] = {0,0,0,0,0,0,0,0};
+
+void setup() {
+  pinMode(DATA, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  writereg();
+}
+
+void loop() {
+  for(int i = 0; i < 9; i++) {
+    registers[i] = HIGH;
+    writereg();
+    delay(100);
+  }
+  for(int i = 8; i >= 0; i--) {
+    registers[i] = LOW;
+    writereg();
+    delay(100);
+  }
+}
+
+void writereg() {
+  digitalWrite(LATCH, LOW);
+  for(int i = 7; i >= 0; i--) {
+    digitalWrite(CLOCK, LOW);
+    digitalWrite(DATA, registers[i]);
+    digitalWrite(CLOCK, HIGH);
+  }
+  digitalWrite(LATCH, HIGH);
+}
